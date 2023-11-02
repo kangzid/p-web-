1e7392f64830bc9aca54a1a3eb031708b6849671
kangzid <zidanalmubarok652@gmail.com>
2023-11-02 08:28:05 +0700

    pwb

---
+++ script.js
@@ -0,0 +1,14 @@
+    $(document).ready(function() {
+        $('.has-animation').each(function(index) {
+        $(this).delay($(this).data('delay')).queue(function(){
+            $(this).addClass('animate-in');
+        });
+        });
+    });
+
+
+    
+
+
+
+
